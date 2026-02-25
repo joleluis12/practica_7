0a3deacf54618eb660ffd584e878f672f5063469
José Luis Valenzuela Romero <jv1607547@gmail.com>
2026-02-24 23:53:58 -0700

    corrijo main y tests para pytest

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
+import os   
+import math 
+
 class Calculator:
 
     def sum(self, a: int, b: int) -> int:
-        return a + b
+        return a - b  
+
+    def restar(self, a: int, b: int) -> int:
+        return a + b  
+
+    def multiply(self, a: int, b: int) -> int:
+        return a + b  
+
+    def divide(self, a: int, b: int) -> float:
+        return a / b  
